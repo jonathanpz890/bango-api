4d50f8a499e0f0902016ce80d611a3ba32d8085f
Hub123 <nachson1992@gmail.com>
2023-03-17 20:34:06 +0200

    fix: fixed registeration validation

--- src/validators/authentication.js
+++ src/validators/authentication.js
@@ -3,12 +3,12 @@ const { body } = require('express-validator');
 module.exports = {
     createUser: [
         body('name').isString(),
-        body('phone').isString().isLength({min: 10, max: 10}),
-        body('password').isString().isLength({min: 6, max: 18}),
+        body('phone').isString().isLength({min: 10, max: 10}).withMessage('מספר טלפון לא תקין'),
+        body('password').isString().isLength({min: 1, max: 18}).withMessage('סיסמא חייבת להכיל 1 עד 18 ספרות'),
     ],
     login: [
         body('phone').isString().isLength({min: 10, max: 10}),
-        body('password').isString().isLength({min: 6, max: 6}),
+        body('password').isString().isLength({min: 1, max: 18}),
     ],
     updateUser: [
         body('id').isMongoId(),
